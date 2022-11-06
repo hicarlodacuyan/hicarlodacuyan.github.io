9092e5b1f7a216f86a7730611345af39dad6d791
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-11-06 17:32:47 +0800

    Add Squada One font and setup default font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        squada: ['"Squada One"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 };
