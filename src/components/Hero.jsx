bcec33c29a7a01f30b5b2cf51383029c283bf29c
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-11-09 10:08:31 +0800

    Fix resume & projects button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,12 +17,14 @@ const Hero = () => {
             and JavaScript, he is always looking to improve his skills and learn
             new technologies.
           </p>
-          <button
-            type="button"
-            className="text-white font-bold uppercase bg-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center self-start transition ease-in-out delay-150 hover:scale-110 duration-200"
-          >
-            <a href="#projects">Projects</a>
-          </button>
+          <a href="#projects">
+            <button
+              type="button"
+              className="text-white font-bold uppercase bg-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center self-start transition ease-in-out delay-150 hover:scale-110 duration-200"
+            >
+              Projects
+            </button>
+          </a>
         </div>
         <div className="flex-1 hidden md:block">
           <img className="" src={HeroImage} />
