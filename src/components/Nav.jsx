724c1dd862a6a7788ff6bb3cb86efe54175853c1
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-11-09 09:21:18 +0800

    Add resume link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,12 @@ const Nav = () => {
             type="button"
             className="text-white font-bold uppercase bg-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 transition ease-in-out delay-150 hover:scale-110 duration-200"
           >
-            Resume
+            <a
+              href="https://drive.google.com/file/d/1iZIgEsg-zVlr4ekW8rOSva-Mfi3eeXIm/view?usp=sharing"
+              target="_blank"
+            >
+              Resume
+            </a>
           </button>
           <button
             onClick={showMobileNav}
